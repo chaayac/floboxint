cdefda21cf30376c1c4f66abb1a6fed8bdabf10d
Christopher Chaaya <z5016114@student.unsw.edu.au>
2016-08-09 23:48:48 +1000

    2nd commit

---
+++ login.php
@@ -0,0 +1,6 @@
+<link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
+<script src="semantic/dist/semantic.min.js"></script>
+
+<div class="ui input">
+  <input type="text" placeholder="Enter your username">
+</div>
